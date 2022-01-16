3b1dbf0b7433a895fca3021b37ecb2b81c0330fe
Faisal Maqbool <faisalmaqbool94gmail.com>
2022-01-17 03:08:24 +0500

    Add last 30 days activities model

---
+++ models/marts/activities/last_30_days_activities_v1.sql
@@ -0,0 +1,16 @@
+/* In the requirement the destination event_name_last_date is expected to be DATE...
+we am just keeping the timestamp (not formatting or parsing it to be DATE), assuming we can do that in our dashboard and keep it as timestamp in case 
+we need to analyze things on timezones. 
+*/
+% set pivot_cols = dbt_utils.get_column_values(table=ref('stg_user_event_v1'), column='event_name') %}
+select 
+
+user_id
+, {% for pivot_col in pivot_cols %}
+    sum(case when event_name = '{{pivot_col}}' then 1 else 0 end)  as `{{pivot_col}}_count`
+    , max(case when event_name = '{{pivot_col}}' then event_datetime else null end) as `{{pivot_col}}_lastUse`
+    {% if not loop.last %}, {% endif%}
+{% endfor %}
+from {{ ref('stg_user_event_v1') }}
+where event_datetime > timestamp_sub(current_timestamp(), interval 30 day)
+group by 1
