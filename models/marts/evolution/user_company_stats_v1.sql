5b55aba5fb027a649c59c537f8298630ed59a215
Faisal Maqbool <faisalmaqbool94gmail.com>
2022-01-17 03:55:05 +0500

    Add user evolution per country model

---
+++ models/marts/evolution/user_company_stats_v1.sql
@@ -0,0 +1,32 @@
+with users_with_company_id as (
+
+select usr_ev.user_id
+, usr.company_id
+, usr_ev.event_datetime
+
+from {{ ref('stg_user_event_v1') }} usr_ev
+inner join {{ ref('stg_users_v1') }} usr
+on usr.user_id = usr_ev.user_id
+
+)
+, member_count_per_company_over_time as (
+
+select 
+format_timestamp("%b'%g", event_datetime) activity_month
+, company_id
+, count(distinct user_id) member_count
+
+from users_with_company_id
+group by 1
+, 2
+)
+select 
+activity_month
+, company_id
+, case
+when member_count < 10 then 'tier C'
+when member_count >= 10 and member_count < 50 then 'tier B'
+else 'tier A'
+end as company_tier
+, member_count
+from member_count_per_company_over_time
