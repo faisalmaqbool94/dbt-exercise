8ad327fd782311968e6fab3b44eea2ce0c14d5a5
Faisal Maqbool <faisalmaqbool94gmail.com>
2022-01-17 03:09:52 +0500

    Fix source reference

--- models/staging/gitbook_exercise/stg_user_event_v1.sql
+++ models/staging/gitbook_exercise/stg_user_event_v1.sql
@@ -9,7 +9,7 @@ user_id
 , event_name
 , event_datetime
 , _extracted_at
-from {{ source('gb_exercise', 'user_events') }}
+from {{ source('gb_exercise', 'user_event') }}
 {% if is_incremental() %}
 where
 Timestamp > (select max(_extracted_at) max_extracted_at from {{ this }})
