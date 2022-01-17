b84bfbd997490da5b0ea0ab2a0bf323a6ca85f00
Faisal Maqbool <faisalmaqbool94@gmail.com>
2022-01-17 12:42:01 +0500

    Delete update_from_template.sh

--- update_from_template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# to replicate in case of recreation as template
-if [ "x$(git remote|grep upstream)" == "x" ] ; then
-    git remote add upstream git@github.com:faisalmaqbool94/dbt-exercise.git
-fi
-git pull upstream main
